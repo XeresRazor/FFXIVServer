c256a75285235c67f44b14fe549d492980e9df00
David Green <dagre@microsoft.com>
2016-02-22 00:21:02 -0500

    Initial Commit

---
+++ FFXIVServer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FFXIVServer
+//
+//  Created by David Green on 2/22/16.
+//  Copyright © 2016 David Green. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
